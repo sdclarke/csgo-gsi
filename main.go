bae9c7febf0cf07e222b829c11b56f03b392ea46
Scott Clarke <sdclarke123@gmail.com>
2021-12-24 21:17:34 +0000

    Remove filesystem exploration feature

--- main.go
+++ main.go
@@ -3,14 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"html"
-	"io"
 	"log"
 	"net/http"
-	"net/url"
-	"os"
-	"sort"
-	"strings"
 
 	"github.com/sdclarke/csgo-gsi/pkg/structs"
 )
@@ -22,9 +16,6 @@ func handle(w http.ResponseWriter, r *http.Request) {
 		d := json.NewDecoder(r.Body)
 		var i structs.State
 		d.Decode(&i)
-		//for k, v := range i {
-		//log.Printf("%s: %#v", k, v)
-		//}
 		if i.Provider != nil {
 			log.Printf("Provider: %#v", *i.Provider)
 		}
@@ -50,75 +41,9 @@ func handle(w http.ResponseWriter, r *http.Request) {
 			log.Printf("Auth: %#v", *i.Auth)
 		}
 		w.WriteHeader(http.StatusOK)
-	case http.MethodGet:
-		dir := http.Dir(os.Getenv("HOME"))
-		path := html.EscapeString(r.URL.Path)
-		f, err := dir.Open(path)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintf(w, "File not found: %s\n", path)
-			return
-		}
-		stat, err := f.Stat()
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if stat.IsDir() {
-			files, err := f.Readdir(0)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			fmt.Fprintf(w, "<table>")
-			fmt.Fprintf(w, "<tr><td><a href=\"..\">..</a></td></tr>")
-			sort.Slice(files, func(i, j int) bool { return files[i].Name() < files[j].Name() })
-			for _, dirFile := range files {
-				name := dirFile.Name()
-				if strings.HasPrefix(name, ".") && r.URL.Query().Get("showHidden") != "true" {
-					continue
-				}
-				fmt.Fprintf(w, "<tr>")
-				fmt.Fprintf(w, "<td>%6d</td>", dirFile.Size())
-				if dirFile.IsDir() {
-					name = fmt.Sprintf("%s/", name)
-				}
-				url := url.URL{Path: name}
-				if path == "/" {
-					fmt.Fprintf(w, "<td><a href=\"%s\">%s</a></td>", url.String(), name)
-				} else {
-					fmt.Fprintf(w, "<td><a href=\"%s%s\">%s</a></td>", path, url.String(), name)
-				}
-				fmt.Fprintf(w, "<td>%s</td>", dirFile.ModTime().Format("01/02/2006 15:04:05 MST"))
-				fmt.Fprintf(w, "</tr>")
-			}
-			fmt.Fprintf(w, "</table>")
-		} else {
-			size := stat.Size()
-			buf, err := io.ReadAll(f)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			if n, err := w.Write(buf); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			} else if int64(n) != size {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-		}
-	//case http.MethodDelete:
-	//path := html.EscapeString(r.URL.Path)
-	//err := os.Remove(strings.TrimPrefix(path, "/"))
-	//if err != nil {
-	//w.WriteHeader(http.StatusNotFound)
-	//fmt.Fprintf(w, "File not found: %s\n", path)
-	//return
-	//}
-	//fmt.Fprintf(w, "Deleted file: %s\n", path)
 	default:
 		fmt.Fprintf(w, "No\n")
+		w.WriteHeader(http.StatusNotImplemented)
 	}
 }
 
